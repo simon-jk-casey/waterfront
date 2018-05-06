5dda48801572b79d807d320f601a14cee68b9121
simon-jk-casey <jesusiwasevil@gmail.com>
2018-05-06 15:08:58 +1200

    reorg folder structure

--- App.js
+++ App.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import * as questions from './src/lib/questions/sectionOne'
 
 export default class App extends React.Component {
   render() {
-    const question = questions.q1a
     return (
       <View style={styles.container}>
-        <Text>{question.title}</Text>
-        <Text>{question.responses[0].response}</Text>
+        <Text>placeholding</Text>
+        <Text>shitposting</Text>
         <Text>Shake your phone to open the developer menu.</Text>
       </View>
     );
