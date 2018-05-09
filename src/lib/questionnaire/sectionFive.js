1c7f7063cf18b42387191a6af12af75daddf51ca
simon-jk-casey <jesusiwasevil@gmail.com>
2018-05-09 22:31:01 +1200

    add partial section 5

---
+++ src/lib/questionnaire/sectionFive.js
@@ -0,0 +1,49 @@
+export const q5a = {
+  qNo: 'Q5a',
+  title: 'How big is your stream?',
+  responses: [
+    {
+      score: 32,
+      response: 'Large lowland river, 15-100m wide.'
+    },
+    {
+      score: 16,
+      response: 'Small river, 5-15m wide.'
+    },
+    {
+      score: 8,
+      response: 'Medium stream, 2-5m wide'
+    },
+    {
+      score: 4,
+      response: 'Small stream, 0-2m wide'
+    }
+  ]
+}
+
+export const q5b = {
+  qNo: 'Q5b',
+  title: 'How deeply incised are your stream banks?',
+  responses: [
+    {
+      score: 16,
+      response: 'Top of stream banks 10m or higher above stream.'
+    },
+    {
+      score: 8,
+      response: 'Top of stream banks 10-5m above stream.'
+    },
+    {
+      score: 4,
+      response: 'Top of stream banks 5-1m above stream.'
+    },
+    {
+      score: 2,
+      response: 'Top of stream banks 1m or less above stream.'
+    }
+  ]
+}
+
+export const q5c = {
+  
+}
