3cfa55c3653ff488dbe3be27c3789c2c91d877e7
simon-jk-casey <jesusiwasevil@gmail.com>
2018-06-30 13:55:00 +1200

    questions progress on button press and append survey state, breaking at end of survey

--- src/components/form.js
+++ src/components/form.js
@@ -7,56 +7,81 @@ export default class Form extends React.Component {
   constructor (props) {
     super(props)
     this.state = {
+      surveyComplete: false,
       index: 0,
-      survey: {}
+      survey: []
     }
   }
   // functions
   buttonHandler (qNo, score) {
-    Alert.alert('touched')
+    // Alert.alert('touched')
     let scoreObj = {}
-    scoreObj[qNo]=score
-    console.log(scoreObj)
-  }
-  nextQuestion () {
+    scoreObj[qNo] = score
+    console.log('scoreObject: ', scoreObj)
+    // this.setState({survey: scoreObj})
+    this.setState(prevState => ({
+      survey: [...prevState.survey, scoreObj]
+    }))
     if (this.state.index < questions.length) {
       this.setState({index: this.state.index + 1})
     }
+  }
+  nextQuestion () {
+    if (this.state.index < questions.length - 1) {
+      this.setState({index: this.state.index + 1})
+    } else {
+      this.setState({surveyComplete: true})
+    }
     // breaks after last question fix later
   }
+  submitScore () {
+    console.log('Submitting: ', this.state)
+  }
+
   render () {
     let displayedQuestion = questions[this.state.index]
     console.log(this.state)
-    return (
-      <View>
-        <Text>{displayedQuestion.title}</Text>
-        {displayedQuestion.responses.map(response => {
-          if (response.response === null) {
-            return (
-              <Button
-                key={response.score}
-                title='placeholder'
-                value={response.score}
-                onPress={() => this.buttonHandler(displayedQuestion.qNo, response.score)}
-              />
-            )
-          } else {
-            return (
-              <Button
-                key={response.score}
-                title={response.response}
-                value={response.score}
-                onPress={this.buttonHandler}
-              />
-            )
-          }
-        })}
-        <Button
-          onPress={this.nextQuestion.bind(this)}
-          title='Next'
-          color='#841584'
-        />
-      </View>
-    )
+    if (!this.state.surveyComplete) {
+      return (
+        <View>
+          <Text>{displayedQuestion.title}</Text>
+          {displayedQuestion.responses.map(response => {
+            if (response.response === null) {
+              return (
+                <Button
+                  key={response.score}
+                  title='placeholder'
+                  value={response.score}
+                  onPress={() => this.buttonHandler(displayedQuestion.qNo, response.score)}
+                />
+              )
+            } else {
+              return (
+                <Button
+                  key={response.score}
+                  title={response.response}
+                  value={response.score}
+                  onPress={() => this.buttonHandler(displayedQuestion.qNo, response.score)}
+                />
+              )
+            }
+          })}
+          <Button
+            onPress={this.nextQuestion.bind(this)}
+            title='Next'
+            color='#841584'
+          />
+        </View>
+      )
+    } else {
+      return (
+        <View>
+          <Button
+            title='submitScore'
+            onPress={() => this.submitScore}
+          />
+        </View>
+      )
+    }
   }
 }
